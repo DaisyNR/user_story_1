d60301e8f5333ba9572076b116b3c1de9e4fe607
Daisy Nardella <daisy50@live.it>
2026-01-06 17:28:58 +0100

    aggiustato grid system

--- resources/views/article/show.blade.php
+++ resources/views/article/show.blade.php
@@ -1,52 +1,63 @@
 <x-layout>
     <div class="container">
-        <div class="row justify-content-center align-items-center text-center mt-5">
-            <h1 class="display-4">
-                {{$article->title}}
-            </h1>
+
+        <div class="row justify-content-center text-center mt-5">
+            <div class="col-12">
+                <h1 class="display-4">
+                    {{ $article->title }}
+                </h1>
+            </div>
         </div>
-        
-        <div class="row justify-content-center p-4">
-            <div class="col-12 col-md-6 mb-3">
-                @if($article->images->count() > 0)
 
-                <div id="carouselExample" class="carousel slide">
-                    <div class="carousel-inner">
-                        @foreach ($article->images as $key=>$image)
-                        <div class="carousel-item @if ($loop->first) active @endif">
-                            <img src="{{Storage::url($image->path)}}" alt="Immagine {{$key +1 }} dell'articolo {{$article->title}}" class="d-block w-100 rounded shadow">
+        <div class="row align-items-start mt-4">
+
+            <div class="col-12 col-md-6">
+                <h4 class="fw-bold">Price</h4>
+                <p class="fs-4">{{ $article->price }} €</p>
+
+                <h4 class="fw-bold">Description</h4>
+                <p>{{ $article->description }}</p>
+            </div>
+
+            {{-- IMMAGINI --}}
+            <div class="col-12 col-md-6">
+
+                @if ($article->images->count() > 0)
+                    <div id="carouselExample" class="carousel slide">
+                        <div class="carousel-inner">
+                            @foreach ($article->images as $key => $image)
+                                <div class="carousel-item @if($loop->first) active @endif">
+                                    <img
+                                        src="{{ Storage::url($image->path) }}"
+                                        class="d-block w-100 rounded shadow"
+                                        alt="Immagine {{ $key + 1 }} dell'articolo {{ $article->title }}"
+                                    >
+                                </div>
+                            @endforeach
                         </div>
-                        @endforeach  
+
+                        @if ($article->images->count() > 1)
+                            <button class="carousel-control-prev" type="button"
+                                    data-bs-target="#carouselExample" data-bs-slide="prev">
+                                <span class="carousel-control-prev-icon"></span>
+                                <span class="visually-hidden">Previous</span>
+                            </button>
+
+                            <button class="carousel-control-next" type="button"
+                                    data-bs-target="#carouselExample" data-bs-slide="next">
+                                <span class="carousel-control-next-icon"></span>
+                                <span class="visually-hidden">Next</span>
+                            </button>
+                        @endif
                     </div>
-
-                    @if ($article->images->count() > 1)
-                    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExample" data-bs-slide="prev">
-                        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                        <span class="visually-hidden">Previous</span>
-                    </button>
-                    <button class="carousel-control-next" type="button" data-bs-target="#carouselExample" data-bs-slide="next">
-                        <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                        <span class="visually-hidden">Next</span>
-                    </button>
-                        
-                    @endif
-                </div>
-
                 @else
-
-                    <img src="https://picsum.photos/300" class="d-block w-100 rounded shadow" alt="No photo provided">
+                    <img src="https://picsum.photos/500"
+                         class="d-block w-50 rounded shadow"
+                         alt="No photo provided">
                 @endif
-                </div>
+
             </div>
 
-            <div class="col-12 col-md-6 text-center">
-                <h2 class="display-5"><span class="fw-bold">Title: </span>{{$article->title}}</h2>
-                <div class="d-flex flex-column justify-content-center h-75">
-                    <h4 class="fw-bold">Price: {{$article->price}}€</h4>
-                    <h5>Description:</h5>
-                    <p>{{$article->description}}</p>
-                </div>
-            </div>
         </div>
     </div>
 </x-layout>
