c713c194c5451fd098fa58734ec29854e1280cf0
Daisy Nardella <daisy50@live.it>
2026-02-12 00:41:36 +0100

    us9 completata

--- app/Jobs/ResizeImage.php
+++ app/Jobs/ResizeImage.php
@@ -4,6 +4,7 @@ namespace App\Jobs;
 
 // use App\Models\Image;
 use Spatie\Image\Image;
+use Spatie\Image\Enums\Unit;
 use Spatie\Image\Enums\CropPosition;
 use Illuminate\Foundation\Queue\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
@@ -34,6 +35,16 @@ class ResizeImage implements ShouldQueue
         $srcPath = storage_path() . '/app/public/' . $this->path . '/' . $this->fileName;
         $destPath = storage_path() . '/app/public/' . $this->path . "/crop_{$w}x{$h}_" . $this->fileName;
 
-        Image::load($srcPath)->crop($w,$h, CropPosition::Center)->save($destPath);
+        Image::load($srcPath)
+        ->crop($w,$h, CropPosition::Center)
+        ->watermark(
+            base_path('public/media/face.png'),
+            width: 50,
+            height:50,
+            paddingX:5,
+            paddingY:5,
+            paddingUnit : Unit::Percent
+        )
+        ->save($destPath);
     }
 }
